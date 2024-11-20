414181c6d2f186d9fb46f54e7ba75070bc03cf8c
gonpampinaicore <gonzalo.pampin@aicore.com.ar>
2024-11-19 23:38:14 -0300

    CRUD Cliente

--- DAL/ClienteData.cs
+++ DAL/ClienteData.cs
@@ -1,12 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entity;
+using Mapper;
 
 namespace DAL
 {
     public class ClienteData
     {
+            string conexionDB = ConfigationDB.ObtenerConexionDB().ToString();
+        public void AgregarCliente(Cliente cliente)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(conexionDB))
+                {
+                    conexion.Open();
+
+                    string query = "INSERT INTO CLIENTE (DNI, NOMBRE, APELLIDO, TELEFONO, TELEFONO_EMERGENCIA, EMAIL, BREVET, ACTIVO) " +
+                                   "VALUES (@Dni, @Nombre, @Apellido, @Telefono, @TelefonoEmergencia, @Email, @Brevet, @Activo)";
+
+                    using (SqlCommand command = new SqlCommand(query, conexion))
+                    {
+                        ClienteMap.MapearCliente(command, cliente);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al agregar cliente: " + ex.Message, ex);
+            }
+        }
+
+        public Cliente ObtenerClientePorId(int id)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(conexionDB))
+                {
+                    conexion.Open();
+
+                    string query = "SELECT ID_CLIENTE, DNI, NOMBRE, APELLIDO, TELEFONO, TELEFONO_EMERGENCIA, EMAIL, BREVET, ACTIVO " +
+                                   "FROM CLIENTE WHERE ID_CLIENTE = @Id";
+
+                    using (SqlCommand command = new SqlCommand(query, conexion))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                return ClienteMap.MapearDesdeReader(reader);
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener cliente por ID: " + ex.Message, ex);
+            }
+        }
+
+        public List<Cliente> ObtenerTodos()
+        {
+            try
+            {
+                List<Cliente> clientes = new List<Cliente>();
+
+                using (SqlConnection conexion = new SqlConnection(conexionDB))
+                {
+                    conexion.Open();
+
+                    string query = "SELECT ID_CLIENTE, DNI, NOMBRE, APELLIDO, TELEFONO, TELEFONO_EMERGENCIA, EMAIL, BREVET, ACTIVO " +
+                                   "FROM CLIENTE";
+
+                    using (SqlCommand command = new SqlCommand(query, conexion))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var cliente = ClienteMap.MapearDesdeReader(reader);
+                                clientes.Add(cliente);
+                            }
+                        }
+                    }
+                }
+                return clientes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener todos los clientes: " + ex.Message, ex);
+            }
+        }
+
+        public void EliminarCliente(int id)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(conexionDB))
+                {
+                    conexion.Open();
+
+                    string query = "DELETE FROM CLIENTE WHERE ID_CLIENTE = @Id";
+                    using (SqlCommand command = new SqlCommand(query, conexion))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al eliminar cliente: " + ex.Message, ex);
+            }
+        }
+
+        public void ActualizarCliente(Cliente cliente)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(conexionDB))
+                {
+                    conexion.Open();
+
+                    string query = "UPDATE CLIENTE SET DNI = @Dni, NOMBRE = @Nombre, APELLIDO = @Apellido, TELEFONO = @Telefono, " +
+                                   "TELEFONO_EMERGENCIA = @TelefonoEmergencia, EMAIL = @Email, BREVET = @Brevet, ACTIVO = @Activo " +
+                                   "WHERE ID_CLIENTE = @Id";
+
+                    using (SqlCommand command = new SqlCommand(query, conexion))
+                    {
+                        ClienteMap.MapearCliente(command, cliente);
+                        command.Parameters.AddWithValue("@Id", cliente.IdCliente);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al actualizar cliente: " + ex.Message, ex);
+            }
+        }
     }
 }
--- Mapper/ClienteMap.cs
+++ Mapper/ClienteMap.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Entity;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,31 @@ namespace Mapper
 {
     public class ClienteMap
     {
+        public static void MapearCliente(SqlCommand command, Cliente cliente)
+        {
+            command.Parameters.AddWithValue("@Dni", cliente.Dni);
+            command.Parameters.AddWithValue("@Nombre", cliente.Nombre);
+            command.Parameters.AddWithValue("@Apellido", cliente.Apellido);
+            command.Parameters.AddWithValue("@Telefono", cliente.Telefono);
+            command.Parameters.AddWithValue("@TelefonoEmergencia", cliente.TelefonoEnergencia);
+            command.Parameters.AddWithValue("@Email", cliente.Email);
+            command.Parameters.AddWithValue("@Brevet", cliente.Brevet);
+        }
+
+        public static Cliente MapearDesdeReader(SqlDataReader reader)
+        {
+            return new Cliente
+            {
+                IdCliente = reader.GetInt32(reader.GetOrdinal("ID_CLIENTE")),
+                Dni = reader.GetInt32(reader.GetOrdinal("DNI")),
+                Nombre = reader.GetString(reader.GetOrdinal("NOMBRE")),
+                Apellido = reader.GetString(reader.GetOrdinal("APELLIDO")),
+                Telefono = reader.GetString(reader.GetOrdinal("TELEFONO")),
+                TelefonoEnergencia = reader.GetString(reader.GetOrdinal("TELEFONO_EMERGENCIA")),
+                Email = reader.GetString(reader.GetOrdinal("EMAIL")),
+                Brevet = reader.GetString(reader.GetOrdinal("BREVET")),
+                Activo = reader.GetBoolean(reader.GetOrdinal("ACTIVO"))
+            };
+        }
     }
 }
